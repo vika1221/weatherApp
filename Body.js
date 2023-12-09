031a3f0fce3c39c14bada9069c3105fb24c31c1f
Vikash <vikashranjanofficial@gmail.com>
2023-12-09 21:17:06 +0530

    updated

--- Body.js
+++ Body.js
@@ -1,19 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const Body=()=>{
-    async function getapi(){
-        const data = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
-        const json = await data.json();
-        console.log(json);
+const Body = () => {
+  const [city, setCity] = useState("");
+  //let city="kolkata";
+  //let key=b70b0cb77dcd9425eb08ff89efedb077;
+  async function getapi(props) {
+    console.log(props);
+    const data = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+props+"&appid=b70b0cb77dcd9425eb08ff89efedb077");
+    const json = await data.json();
+    console.log(json);
 
-    }
+    return json;
+  }
 
-    useEffect(()=>{getapi()},[]);
-    return(
-        <>
-            <div className="body">Body</div>
-        </>
-    )
-}
+  return (
+    <>
+      <input
+        type="text"
+        className="search"
+        placeholder="Enter the city Name"
+        value={city}
+        onChange={(e) => {
+          setCity(e.target.value);
+        }}/>
+      <button onClick={()=>{
+        getapi(city);
+      }}>
+        search
+      </button>
+      <h1></h1>
+      <div className="body"> Body</div>  
+    </>
+  );
+};
 
 export default Body;
